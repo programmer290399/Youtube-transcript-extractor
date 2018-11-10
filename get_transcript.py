e06dd75548bd4c6a8027cbc7dc6a144bd982df9f
Saahil Ali <programmer290399@gmail.com>
2018-11-10 12:52:20 +0530

    Integrated search option

--- get_transcript.py
+++ get_transcript.py
@@ -2,6 +2,22 @@ import re
 from urllib.parse import urlparse
 from youtube_transcript_api import YouTubeTranscriptApi
 from urllib.parse import parse_qs
+import urllib.request
+from bs4 import BeautifulSoup
+
+out_file = open("urls.txt", "w+")
+
+
+textToSearch = input('enter query')
+query = urllib.parse.quote(textToSearch)
+url = "https://www.youtube.com/results?search_query=" + query
+response = urllib.request.urlopen(url)
+html = response.read()
+soup = BeautifulSoup(html, 'html.parser')
+for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+    print("Extracting:"+'https://www.youtube.com' + vid['href'] )
+    out_file.write('https://www.youtube.com' + vid['href'] + '\n')
+out_file.close()
 
 
 
@@ -35,11 +51,14 @@ with open('urls.txt', 'r', encoding="utf-8") as f:
             video_id = video_id_extractor(url)
             print('Retriving :', video_id)
             if video_id is not None :
-                transcript_dirty  = YouTubeTranscriptApi.get_transcript(video_id)
+                try :
+                    transcript_dirty  = YouTubeTranscriptApi.get_transcript(video_id)
 
-                transcript_clean = str()
+                    transcript_clean = str()
 
-                for parts in transcript_dirty :
-                    transcript_clean += (" " + parts['text'])
-                print('writing to file ...')
-                fh.write(transcript_clean + '\n')
+                    for parts in transcript_dirty :
+                        transcript_clean += (" " + parts['text'])
+                    print('writing to file ...')
+                    fh.write(transcript_clean + '\n')
+                except :
+                    print('Error on :', url )
