b1419078c1a036b66d86cdda5cec651b77a681c4
Saahil Ali <programmer290399@gmail.com>
2018-11-10 12:01:45 +0530

    First Commit

---
+++ get_transcript.py
@@ -0,0 +1,45 @@
+import re 
+from urllib.parse import urlparse
+from youtube_transcript_api import YouTubeTranscriptApi
+from urllib.parse import parse_qs
+
+
+
+
+def video_id_extractor(value):
+
+    query = urlparse(value)
+    if query.hostname == 'youtu.be':
+        return query.path[1:]
+    if query.hostname in ('www.youtube.com', 'youtube.com'):
+        if query.path == '/watch':
+            p = parse_qs(query.query)
+            return p['v'][0]
+        if query.path[:7] == '/embed/':
+            return query.path.split('/')[2]
+        if query.path[:3] == '/v/':
+            return query.path.split('/')[2]
+    
+    return None
+
+
+
+
+
+
+fh = open('youtube_data.txt','w+', encoding='utf-8')
+with open('urls.txt', 'r', encoding="utf-8") as f:
+    for url in f:
+        
+    
+            video_id = video_id_extractor(url)
+            print('Retriving :', video_id)
+            if video_id is not None :
+                transcript_dirty  = YouTubeTranscriptApi.get_transcript(video_id)
+
+                transcript_clean = str()
+
+                for parts in transcript_dirty :
+                    transcript_clean += (" " + parts['text'])
+                print('writing to file ...')
+                fh.write(transcript_clean + '\n')
